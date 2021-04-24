5d70f9301ed72926ec6cb7907f162164ab23d2c2
Andrei Vaduva <andrei.vaduva@gmail.com>
2021-04-24 17:29:03 +0300

    Fixing WSS

---
+++ src/modules/transports/wss/peer.js
@@ -0,0 +1,35 @@
+const AbstractPeer = require("../abstract/AbstractPeer");
+
+module.exports = class WSPeer extends AbstractPeer {
+    onConnect() {
+        this.socket.on("close", () => {
+            this.disconnect("Remote Connection Closed");
+        });
+
+        this.socket.on("error", (err) => {
+            this.emit("error", this, err);
+        });
+
+        this.socket.on("message", (message) => {
+            this.emit("message", this, message);
+        });
+    }
+
+    send(message) {
+        return new Promise((resolve, reject) => {
+            try {
+                this.socket.send(message);
+                resolve();
+            } catch (e) {
+                reject(e);
+            }
+        });
+    }
+
+    onDisconnect() {
+        if (this.socket) {
+            this.socket.close();
+            this.socket = null;
+        }
+    }
+};
--- src/modules/transports/wss/transport.js
+++ src/modules/transports/wss/transport.js
@@ -1,7 +1,59 @@
-const WSTransport = require("../ws/transport");
-class WSS extends WSTransport {
+const AbstractTransport = require("../abstract/AbstractTransport");
+const Peer = require("./peer.js");
+const WebSocket = require("ws");
+
+class WSS extends AbstractTransport {
+    constructor(type, options) {
+        super(type, options);
+    }
+
     static scheme() {
-        return "ws";
+        return "wss";
+    }
+
+    onListen() {
+        return new Promise((resolve, reject) => {
+            this.ws = new WebSocket.Server({
+                port: this.options.port,
+                host: this.options.ip
+            });
+            this.ws.on('connection', socket => {
+                this.addPeer(new Peer(socket));
+            });
+
+            this.ws.on('error', err => {
+                reject(err);
+            });
+
+            this.ws.on('listening', () => {
+                resolve();
+            });
+        });
+    }
+
+    onConnect() {
+        return new Promise((resolve, reject) => {
+            let ws = new WebSocket(WSS.scheme() + '://' + this.options.ip + ':' + this.options.port + '/');
+
+            ws.on('open', socket => {
+                this.addPeer(new Peer(ws));
+                resolve();
+            });
+
+            ws.on('error', err => {
+                reject(err);
+            });
+        });
+    }
+
+    onStop() {
+        return new Promise((resolve, reject) => {
+            if (this.ws) {
+                this.ws.close();
+                this.ws = null;
+            }
+            resolve();
+        });
     }
 }
 module.exports = WSS;
